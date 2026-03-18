a233171e95158d189336c0b39aa96be92f9506a5
lvalencia-gif <lvalencia@wmpcs.org>
2026-03-18 14:43:00 +0000

    fix

--- import random.py
+++ import random.py
@@ -1,14 +1,13 @@
 import random
-
 print("Hi! Let's play a guess-the-number game.")
 secret = random.randint(1, 20)
 
-while True: and a dog with a log
+while True:
     guess = int(input("Guess a number 1-20: "))
     if guess < secret:
         print("Too low.")
     elif guess > secret:
         print("Too high.")
-    else:a cat with a hat
+    else:
         print("You got it! 🎉")
         break
