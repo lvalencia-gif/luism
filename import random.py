4967aac72ddc58d5567f54e4a27d6359db8ce818
lvalencia-gif <lvalencia@wmpcs.org>
2026-03-23 14:18:52 +0000

    almost

--- import random.py
+++ import random.py
@@ -1,5 +1,14 @@
 import random
 
+def to_roman(num):
+    roman_numerals = {
+        1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V',
+        6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX', 10: 'X',
+        11: 'XI', 12: 'XII', 13: 'XIII', 14: 'XIV', 15: 'XV',
+        16: 'XVI', 17: 'XVII', 18: 'XVIII', 19: 'XIX', 20: 'XX'
+    }
+    return roman_numerals.get(num, str(num))
+
 print("Hi! Let's play a guess-the-number game.")
 secret = random.randint(1, 20)
 
@@ -18,9 +27,9 @@ while True:
     elif guess > secret:
         print("Too high.")
     else:
-        print("You got it! 🎉")
+        print(f"You got it! The number was {to_roman(secret)}. 🎉")
         break
 
 print("Rolling a dice...")
 roll = random.randint(1, 6)
-print("You rolled:", roll)
+print("You rolled:", to_roman(roll))
